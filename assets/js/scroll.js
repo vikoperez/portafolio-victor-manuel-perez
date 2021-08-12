985995f35193a6740be916e45358ddcaa05e4867
vikoperez <k-kratoss@hotmail.com>
2021-08-11 22:12:22 -0500

    borre contacto en portafolio

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,1 +1,10 @@
-alert("si funciona")
+const header = document.getElementById("main-header");
+
+function scrollHeaderTop (){
+    if (window.scrollY > 500){
+        header.classList.add("top-header")
+    }
+}
+
+document.addEventListener("scroll", scrollHeaderTop);
+// document.onscroll=scrollHeaderTop
